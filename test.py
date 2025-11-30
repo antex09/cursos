ec34b23ffb080c4da3ce01eedef077d47094a52f
nosl <migu13@gmail.com>
2025-11-30 13:11:55 -0300

    Oh

--- test.py
+++ test.py
@@ -1,2 +1,20 @@
+# print("Hola a todos, es solo tipo test!")
+message = "\t\tHola desde Python\n"
+message2 = "\t\t¡Adios mundo cruel!\n"
 
-print("Hola a todos, es solo tipo test!")
+print(message + message2)
+
+message3 = "¡Hola!"
+mi_nombre = " Mi nombre es Burgz."
+
+# print(message3 + mi_nombre)
+
+my_name = " Mi nombre es \
+Nègre.\n"
+
+print(message3 + my_name)
+
+b = 0
+for i in range (5):
+  b += 1
+  print(b)
